037cc36e38c5e3ca9bc15ef4c3a263866b704568
Cova8bitdot <cova.tech.8bitdots@gmail.com>
2020-07-06 11:35:35 +0900

    フォルダ名リネーム

---
+++ UnitySound/Scripts/Sound/Editor/SoundItemEditor.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace CovaTech.EditorTool
+{
+    using CovaTech.UnitySound;
+
+    [CustomEditor(typeof(SoundItem))]
+    public class SoundItemEditor : Editor
+    {
+
+        public void OnEnable()
+        {
+            EditorApplication.update += EditorUpdateSequence;
+        }
+
+        public void OnDisable()
+        {
+            EditorApplication.update -= EditorUpdateSequence;
+
+        }
+
+        void EditorUpdateSequence()
+        {
+            Repaint();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            EditorGUILayout.LabelField("-------------DEBUG-------------");
+
+            var item = target as SoundItem;
+
+            EditorGUILayout.IntField("ID", item.GetHandler());
+            EditorGUILayout.LabelField("[State]");
+            EditorGUI.indentLevel++;
+            {
+                EditorGUILayout.EnumPopup("Current", item.CurrentState);
+                EditorGUILayout.EnumPopup("Prev", item.CurrentState);
+            }
+            EditorGUI.indentLevel--;
+
+            EditorGUILayout.EnumPopup("AUDIO_TYPE", item.AudioType);
+            EditorGUILayout.Toggle("IsFading", item.IsFading);
+            EditorGUILayout.Toggle("IsPlaying", item.IsPlaying);
+            EditorGUILayout.Toggle("IsUsing", item.IsUsed());
+
+        }
+    }
+}
